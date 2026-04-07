a9118d032d5904b1ef55c2912adf5b6476fd8c39
margaretjgu <margaret.gu@elastic.co>
2026-04-07 14:02:47 -0400

    fix error handling empty response

--- src/cloud/apis/projects.ts
+++ src/cloud/apis/projects.ts
@@ -6,7 +6,7 @@
 import type { CloudApiDefinition } from '../types.ts'
 
 const projectIdParam = {
-  name: 'id',
+  name: 'project_id',
   description: 'ID of the Elasticsearch project',
   required: true,
 }
@@ -24,7 +24,7 @@ export const projectApis: CloudApiDefinition[] = [
     namespace: 'projects',
     description: 'Retrieve information about an Elasticsearch serverless project',
     method: 'GET',
-    path: '/api/v1/serverless/projects/elasticsearch/{id}',
+    path: '/api/v1/serverless/projects/elasticsearch/{project_id}',
     pathParams: [projectIdParam],
   },
   {
@@ -32,7 +32,7 @@ export const projectApis: CloudApiDefinition[] = [
     namespace: 'projects',
     description: 'Delete an Elasticsearch serverless project',
     method: 'DELETE',
-    path: '/api/v1/serverless/projects/elasticsearch/{id}',
+    path: '/api/v1/serverless/projects/elasticsearch/{project_id}',
     pathParams: [projectIdParam],
   },
 ]
